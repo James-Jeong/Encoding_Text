44ed5d77bf704fcabb363e03212b689726587825
jamesj <ehddnr177@naver.com>
2019-08-05 16:31:42 +0900

    Modify

--- main.cpp
+++ main.cpp
@@ -1,14 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
-#include "Define.h"
-#include "Type.h"
-#include "UnicodeEncoding.h"
-#include "Unicode.h"
-#include "utf.h"
-#include "unicode_data.h"
-#include "text_encoding_detect.h"
+#include "stdafx.h"
 
 using namespace AutoIt;
 using namespace Common;
@@ -18,50 +8,85 @@ using namespace utf;
 
 int main()
 {
+	/*char line[100];
+
+	FILE* file = fopen("test1.txt", "r");
+	ReadBOM(file);
+	while (fscanf(file, "%s", line)) {
+		cout << line << endl;
+	}
+
+	fclose(file);
+	*/
+
 	string filePath = "test1.txt";
 	int cnt = 1;
-
 	// read File
 	ifstream openFile(filePath.data());
 	ofstream writeFile1("Result_text.txt");
 	if (openFile.is_open()) {
 		string line;
 		string MU8;
+		string MASCII;
+		string MU16;
+		string MU32;
 
 		TextEncodingDetect textDetect;
 		TextEncodingDetect::Encoding encoding = textDetect.DetectEncoding
-		((const unsigned char*)line.c_str(), line.size());
+		((const unsigned char*)(TEXT(line)).c_str(), line.size());
 
 		while (getline(openFile, line)) {
-			encoding_type DTResult = detect_encoding(line); // int DTResult
-			if (DTResult == 0) cout << cnt << "     " << "ENCODING_UNKNOWN" << endl;
-			else if (DTResult == 1) cout << cnt << "     " << "ENCODING_ASCII" << endl;
-			else if (DTResult == 2) cout << cnt << "     " << "ENCODING_UTF8" << endl;
-			else if (DTResult == 3) cout << cnt << "     " << "ENCODING_UTF16BE" << endl;
-			else if (DTResult == 4) cout << cnt << "     " << "ENCODING_UTF16LE" << endl;
-			else if (DTResult == 5) cout << cnt << "     " << "ENCODING_UTF32BE" << endl;
-			else if (DTResult == 6) cout << cnt << "     " << "ENCODING_UTF32LE" << endl;
+			//line.erase(remove(line.begin(), line.end(), NULL), line.end()); // erase null character in string line
+			encoding_type DTResult = detect_encoding(TEXT(line)); // int DTResult
+			//cout << cnt << " = " << textDetect.CheckBOM((const unsigned char*)line.c_str(), line.size()) << endl;
 
-			//else if (encoding == TextEncodingDetect::UTF16_LE_BOM || encoding == TextEncodingDetect::UTF16_LE_NOBOM)
-			//	wprintf(L"UTF-16 Little Endian");
-			//else if (encoding == TextEncodingDetect::UTF16_BE_BOM || encoding == TextEncodingDetect::UTF16_BE_NOBOM)
-			//	wprintf(L"UTF-16 Big Endian");
-			
 			if (encoding == TextEncodingDetect::ANSI) {
-				//wprintf(L"ANSI (chars in the range 0-255 range)");
 				if (line.size() > 3) {
-					cout << cnt << "     " << line << endl;
-					writeFile1 << cnt << "     " << line << endl;
+					cout << cnt << "     " << TEXT(line) << endl;
+					writeFile1 << cnt << "     " << TEXT(line) << endl;
 				}
 			}
+			if (encoding == TextEncodingDetect::ASCII) {
+				if (DTResult == ENCODING_UTF8) { //encoding == TextEncodingDetect::UTF8_BOM || encoding == TextEncodingDetect::UTF8_NOBOM
+					MU8 = MBSFromUTF8(TEXT(line));
+					if (writeFile1.is_open() && !MU8.empty()) {
+						if (MU8.size() > 3) {
+							cout << cnt << "     " << MU8 << endl;
+							writeFile1 << cnt << "     " << MU8 << endl;
+						}
+					}
+				}
+				else if (DTResult == ENCODING_UTF16LE || DTResult == ENCODING_UTF16BE) {
+					MU16 = MBSFromUTF16((const WORD*)WORDFrom(TEXT(line)), line.size());
+					if (writeFile1.is_open() && !MU16.empty()) {
+						if (MU8.size() > 3) {
+							cout << cnt << "     " << MU16 << endl;
+							writeFile1 << cnt << "     " << MU16 << endl;
+						}
+					}
+				}
+				else if (DTResult == ENCODING_UTF32LE || DTResult == ENCODING_UTF32BE) {
+					MU32 = MBSFromUTF32((const DWORD*)DWORDFrom(TEXT(line)), line.size());
+					if (writeFile1.is_open() && !MU32.empty()) {
+						if (MU8.size() > 3) {
+							cout << cnt << "     " << MU32 << endl;
+							writeFile1 << cnt << "     " << MU32 << endl;
+						}
+					}
+				}
+				else {
+					MASCII = MBSFromASCII(TEXT(line));
+					if (writeFile1.is_open() && !MASCII.empty()) {
+						if (MASCII.size() > 3) {
+							cout << cnt << "     " << MASCII << endl;
+							writeFile1 << cnt << "     " << MASCII << endl;
+						}
+					}
+				}
+			}
+
 			
-			if (encoding == TextEncodingDetect::UTF8_BOM || encoding == TextEncodingDetect::UTF8_NOBOM) MU8 = MBSFromUTF8(line);
-			if (writeFile1.is_open() && !MU8.empty()) {
-				if (MU8.size() > 3) {
-					cout << cnt << "     " << MU8 << endl; // 11
-					writeFile1 << cnt << "     " << MU8 << endl;
-				}
-			}
+
 			cnt++;
 		}
 		openFile.close();
